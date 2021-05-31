c19915c75704e830e493e7a596d2ddaedee6cd82
NearFire13 <79142302+NearFire13@users.noreply.github.com>
2021-05-31 12:37:22 +0200

    Rename Src/principal.c to PjtKEIL_StepDeb_2_3/Src/principal.c

--- PjtKEIL_StepDeb_2_3/Src/principal.c
+++ PjtKEIL_StepDeb_2_3/Src/principal.c
@@ -9,17 +9,17 @@ int main(void)
 // ============= INIT PERIPH (faites qu'une seule fois)  =====================
 // ===========================================================================
 
-// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+// AprÃ¨s exÃ©cution : le coeur CPU est clockÃ© Ã  72MHz ainsi que tous les timers
 CLOCK_Configure();
 
-// configuration du Timer 4 en débordement 100ms
+// configuration du Timer 4 en dÃ©bordement 100ms
 Timer_1234_Init_ff(TIM4, 7200000); //Ticks = 100 ms * 72x10^6 = 7200000
 	
 // Activation des interruptions issues du Timer 4
-// Association de la fonction à exécuter lors de l'interruption : timer_callback
-// cette fonction (si écrite en ASM) doit être conforme à l'AAPCS
+// Association de la fonction Ã  exÃ©cuter lors de l'interruption : timer_callback
+// cette fonction (si Ã©crite en ASM) doit Ãªtre conforme Ã  l'AAPCS
 	
-// Activation du Débordement Timer
+// Activation du DÃ©bordement Timer
 Active_IT_Debordement_Timer(TIM4, 2, timer_callback);
 	
 Run_Timer(TIM4);
