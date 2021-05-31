c0df93bc68718e4ce091d75a5b2e93898707012c
NearFire13 <79142302+NearFire13@users.noreply.github.com>
2021-05-31 14:07:08 +0200

    Add 'PjtKEIL_StepDFT' project

---
+++ PjtKEIL_StepDFT/Src/principal.c
@@ -0,0 +1,32 @@
+
+
+#include "DriverJeuLaser.h"
+
+extern short int LeSignal;
+extern int DFT_ModuleAuCarre(short int * Signal64ech, char k);
+int tab[64];
+int main(void)
+{
+
+// ===========================================================================
+// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+// ===========================================================================
+
+// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+CLOCK_Configure();
+
+	
+
+
+//============================================================================	
+
+for(int i=0; i < 64; i++)
+{
+	tab[i] = DFT_ModuleAuCarre(&LeSignal, i); // LeSignal, k
+}	
+	
+while	(1)
+	{
+	}
+}
+
